48c78fd2b1a949ecc31b70f458441b678272ec66
NekoCarreraDesigns <denvermaas@gmail.com>
2020-11-28 12:11:37 -0700

    added sign up button to app, and a paragraph to the sign in, styled button and paragraph, added routes file

--- nong-coding-challenge/src/Components/SignIn/SignIn.js
+++ nong-coding-challenge/src/Components/SignIn/SignIn.js
@@ -3,13 +3,17 @@ import "./SignIn.css"
 
 function SignIn () {
     return (
+        
         <div>
+        <p className="opening-paragraph">Please sign in or if your new here, please sign up!</p>
         <input className="user-email" placeholder="email"></input>
         <br></br>
         <input className="user-password" placeholder="password"></input>
         <br></br>
         <button className="Sign-in-button">Sign In</button>
+        <button className="Sign-up-button">Sign Up</button>
         </div>
+        
     )
 }
 
