e89d6f6e90315a6d33566c13988c48cd67ef054b
NekoCarreraDesigns <denvermaas@gmail.com>
2020-11-29 22:18:30 -0700

    added picture to app, created routes with react router, created signup page

--- nong-coding-challenge/src/App.js
+++ nong-coding-challenge/src/App.js
@@ -1,17 +1,25 @@
 import React from "react";
 import SignIn from "./Components/SignIn/SignIn";
-// import Form from "./Components/Form/Form";
-// import {BrowserRouter as Router, Redirect, Route} from "react-router-dom";
+import SignUp from "./Components/Signup/Signup"
+import Form from "./Components/Form/Form";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <header className="App-header">
   <p><i class="fas fa-tractor"></i>{' '}Crop Scout{' '}<i class="fas fa-tractor"></i></p>
       </header>
-     <SignIn/>
+     <Switch>
+      <Route exact path="/" component={SignIn}></Route>  
+      <Route path="/sign-up" component={SignUp}></Route>  
+      <Route path="/form" component={Form}></Route>  
+     </Switch>     
+     
     </div>
+    </Router>
   );
 }
 
